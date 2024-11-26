7bfbe3fa4c944a531606919a1ef68991de4f2ac7
laptop <2003mohaben@gmail.com>
2024-11-26 01:47:36 +0100

    test

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-//   server : { port: '3000'},  
-  base : '/KoulshiNet/',
+    plugins: [react()],
+    build: {
+        outDir: '../public', // Outputs React build to Laravel's public directory
+        emptyOutDir: true,
+        base : '/KoulshiNet/',
+    },
+    server: {
+        port: 3000, // React development server port
+        proxy: {
+            '/api': 'http://127.0.0.1:8000', // Proxy Laravel API requests
+        },
+    },
 })
